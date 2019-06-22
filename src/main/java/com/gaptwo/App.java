6a23e2e80d438b078d0c762c9bb67381eba4c6b1
harshaubuntu <harshaubuntu@gmail.com>
2019-06-22 07:28:41 +0000

    gaptwo

---
+++ src/main/java/com/gaptwo/App.java
@@ -0,0 +1,13 @@
+package com.gaptwo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
